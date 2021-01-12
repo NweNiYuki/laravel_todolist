1b26f101d1de94695ea4fd7ab58a317b8cb7da24
nwe ni aung <nweni5240@gmail.com>
2021-01-12 16:03:35 +0630

    input feature

---
+++ resources/views/create.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+
+@section('content')
+
+<div class="container">
+	<div class="jumbotron">
+		<form action="" method="post" enctype="multipart/form-data">
+			@csrf
+		<div class="form-group">
+		<label>Title</label>
+		<input type="text" name="title" class="form-control" required>
+		</div>
+		
+		<div class="form-group">
+		<label>Content</label>
+		<textarea type="text" name="content" class="form-control"></textarea>
+		</div>
+
+		<div class="form-group">
+		<label>Reason</label>
+		<textarea type="text" name="reason" class="form-control"></textarea>
+		</div>
+
+	
+		<input class="btn btn-primary" type="submit" value="Add New">
+
+
+	</form>
+	</div>
+
+</div>
+
+
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>TodoList Blog</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    
+    
+</head>
+<body>
+   
+    @yield("content")
+</body>
+</html>
